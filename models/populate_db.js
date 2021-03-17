8e4c7e98e77701fb560108bdc1064758502b9ce7
Raphael Assis <58819530+AssisRaphael@users.noreply.github.com>
2021-03-17 11:32:02 -0300

    Adicionado verificação de agendamento de consulta

---
+++ models/populate_db.js
@@ -0,0 +1,182 @@
+const database = require('./database');
+
+const _medicos = [
+    {
+        nome: "Dr. Sophia Lara Elisa Assunção",
+        email: "sophialara@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Renato Henrique Manoel Araújo",
+        email: "renatohenrique@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761210",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Matheus Ruan Gael da Paz",
+        email: "matheusruan@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50714250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Samuel Roberto Castro",
+        email: "samuelcastro@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761880",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Lucca Julio Anthony das Neves",
+        email: "lucaajulio@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Luna Carla Ferreira",
+        email: "elunacarla@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Beatriz Eliane Tatiane Martins",
+        email: "beatrizeliane@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Guilherme Isaac Ramos",
+        email: "guilhermeisaac@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Augusto Hugo Sales",
+        email: "augustoh@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    },
+    {
+        nome: "Dr. Thiago Lucca Samuel Galvão",
+        email: "thiagoL@alkbrasil.com.br",
+        celular: "81983298535",
+        cep: "50761250",
+        logradouro: "rua são bernardo",
+        bairro: "Terezinha",
+        cidade: "Santa efigênnia",
+        estado: "Bahia",
+        data_contrato: "30-05-2001",
+        salario: Math.random() * 5000,
+        senha_hash: Math.floor(Math.random() * 258963147),
+        especialidade: "Cardiologista",
+        crm: Math.floor(Math.random() * 258963147)
+    }
+];
+
+function CadatsraMedicos() {
+    console.log("hehe")
+    for (medicos of _medicos) {
+        console.log("****"+medicos)
+        database.Medicos.create({
+            nome: medicos.nome,
+            email: medicos.email,
+            telefone: medicos.celular,
+            cep: medicos.cep,
+            logradouro: medicos.logradouro,
+            bairro: medicos.bairro,
+            cidade: medicos.cidade,
+            estado: medicos.estado,
+            data_contrato: new Date("December 17, 1995 03:24:00"),
+            salario: medicos.salario,
+            senha_hash: medicos.senha_hash,
+            especialidade: medicos.especialidade,
+            crm: medicos.crm
+        }).then(function () {
+            console.log(`(${medicos.nome}) - sucesso`)
+        }).catch(function (erro) {
+            console.log(`---- (${medicos.nome}) - ERRO` + erro)
+        })
+    }
+}
+
+//CadatsraMedicos();
--- routes/rotas.js
+++ routes/rotas.js
@@ -6,6 +6,11 @@ const express = require('express')
 const rotas = express.Router()
 //const path = require('path');
 const database = require('../models/database');
+const db = require('../models/bd');
+
+hashCode = function(s){
+    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+}
 
 //Página inicial do site
 rotas.get('/', (req,res) => {
@@ -34,6 +39,36 @@ rotas.get('/cadastro-endereco', (req,res) => {
 
 rotas.get('/agendar-consulta', (req,res) => {
     res.render('agendar-consulta');
+})
+
+rotas.post('/add-consulta',(req,res) => {
+    database.Agendas.findAll({
+        where: {
+            codigo_medico: req.body.medico,
+            horario: req.body.horario,
+          }
+    }).then(function verificaConsulta(agendas){
+        console.log(`\n\n@@@@@@@@@@@@@@@@@@Agendas: ${agendas.length}\n${agendas.toString()}`);  
+        if(agendas.length > 0){
+                res.send("Já existe uma consulta agendada nesse horário, selecione outro horário ou médico.");
+            }
+        else{
+            database.Agendas.create({
+                data: req.body.data,
+                nome: req.body.nome,
+                horario: req.body.horario,
+                codigo_medico: req.body.medico,
+                telefone: req.body.telefone,
+                email: req.body.email,
+            }).then(function (){
+                res.send('Consulta agendada com sucesso!');
+                res.redirect("/")
+            }).catch(function(erro){
+                res.send('Opps, tivemos um erro ao acessar o banco de dados, tente novamente mais tarde.');
+                console.log("erro: "+erro)
+            })
+        }
+    });
 })
 
 rotas.post('/add-endereco',function(req, res){
